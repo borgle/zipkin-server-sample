b309346c174ab0bced8b98cb64358994ca14cae5
Yoker.Wu <Yoker.Wu@gmail.com>
2018-01-09 16:03:57 +0800

    Added custom ZipkinRestTemplateSender for Basic Authorization

--- gateway/src/main/java/com/gkeeps/gateway/Application.java
+++ gateway/src/main/java/com/gkeeps/gateway/Application.java
@@ -1,10 +1,14 @@
 package com.gkeeps.gateway;
 
+import com.gkeeps.gateway.configuration.BasicAuthorizationZipkinRestTemplateCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinProperties;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinRestTemplateCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
@@ -14,5 +18,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    ZipkinRestTemplateCustomizer zipkinRestTemplateCustomizer(ZipkinProperties zipkinProperties) {
+        return new BasicAuthorizationZipkinRestTemplateCustomizer(zipkinProperties);
     }
 }
--- service/src/main/java/com/gkeeps/service/Application.java
+++ service/src/main/java/com/gkeeps/service/Application.java
@@ -1,8 +1,12 @@
 package com.gkeeps.service;
 
+import com.gkeeps.service.configuration.BasicAuthorizationZipkinRestTemplateCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinProperties;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinRestTemplateCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,5 +14,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    ZipkinRestTemplateCustomizer zipkinRestTemplateCustomizer(ZipkinProperties zipkinProperties) {
+        return new BasicAuthorizationZipkinRestTemplateCustomizer(zipkinProperties);
     }
 }
